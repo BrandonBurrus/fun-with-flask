d4f79a8068766a41a16cd027c56fbd4c10a7a2a2
Brandon Burrus <brandon@burrus.io>
2020-03-26 20:39:47 -0700

    feat: added sqlite db + models

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  content = db.Column(db.String(200), nullable=False)
+  completed = db.Column(db.Integer, default=0)
+  date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+  def __repr__(self):
+    return f'<Todo {self.id}'
+
+db.create_all()
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.jinja')
+  if (request.method == 'POST'):
+    todo_content = request.form['content']
+    if (len(todo_content) > 0):
+      new_todo = Todo(content=todo_content)
+
+      try:
+        db.session.add(new_todo)
+        db.session.commit()
+        return redirect('/')
+      except:
+        return redirect('/')
+    return redirect('/')
+  else:
+    todos = Todo.query.order_by(Todo.date_created).all()
+    return render_template('index.jinja', todos=todos)
 
 if __name__ == "__main__":
   app.run(debug=True)
